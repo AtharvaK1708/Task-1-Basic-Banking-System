28b5b8ec6eb4dd4a5da19ebcd2b649e12acc33dd
AtharvaK1708 <atharva.kulkarni21@gmail.com>
2021-12-07 23:37:41 +0530

    --

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
   );
 }
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
